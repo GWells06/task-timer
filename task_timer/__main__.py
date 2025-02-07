50edf09cec1c693c26b95a14f0226115d8875329
GWells06 <102601244+GWells06@users.noreply.github.com>
2025-02-06 22:36:57 -0600

    Update __main__.py

--- task_timer/__main__.py
+++ task_timer/__main__.py
@@ -3,18 +3,15 @@ __main__.py
 Grant Wells <gswells0206@gmail.com>
 <The date in 2025-01-30 format>
  
-<Short Description>
+<Main file for the CLI Task Timer. Imports needed files and runs the program.>
 """ 
 
 # Imports Here: 
 import click
-from interface import formatted_header
+from utils import formatted_header
 from user_input import welcome_to_task_timer
 from user_input import user_task_choice
-from user_input import task_interface
-
-
-#from interface import welcome_to_task_timer()
+from interface import task_interface
 
 @click.command()
 
@@ -22,14 +19,16 @@ def main():
     """This is my main cli."""
 
     # Top Header of Program
-    formatted_header(header_message=" CLI TASK TIMER ")
-    welcome_to_task_timer()
-    task_interface()
-    user_task_choice()
-    # Welcome Section of the Program.
-    
+    formatted_header(" 2077 CLI TASK TIMER ")
 
-    click.echo("Hello World")
+    # Tell the End-User How It Works
+    welcome_to_task_timer()
+    
+    # Interface Housing Tasks
+    task_interface()
+
+    # Operate Based On User Choice
+    user_task_choice()
  
 if __name__ == '__main__':
     main()
